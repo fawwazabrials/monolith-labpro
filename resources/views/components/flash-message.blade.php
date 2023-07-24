22015238f5429fa8d409e7ac36d126280151225c
fawwazabrials <fawwazabrial.fas@gmail.com>
2023-07-24 19:23:55 +0700

    remove flash message after some time

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,5 @@
 @if(session()->has('danger'))
-<div class="alert alert-danger m-0" role="alert">
+<div class="alert alert-danger m-0" x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show"  role="alert">
     <p class="text-center p-0 m-0">
         {{ session()->get("danger") }}
     </p>
@@ -7,7 +7,7 @@
 @endif
 
 @if (session()->has('success'))
-<div class="alert alert-success m-0" role="alert">
+<div class="alert alert-success m-0" x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show"  role="alert">
     <p class="text-center p-0 m-0">
         {{ session()->get("success") }}
 
